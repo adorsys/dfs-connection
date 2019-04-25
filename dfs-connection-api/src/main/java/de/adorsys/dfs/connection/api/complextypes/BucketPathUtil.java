ad76a2a0856b5a72dfb4776164506fb3b5c3a643
electronicpeter <github@electronicpeter.de>
2019-04-25 11:28:23 +0200

    DOC-129 make dfs-connection runnable under windows too

--- dfs-connection-api/src/main/java/de/adorsys/dfs/connection/api/complextypes/BucketPathUtil.java
+++ dfs-connection-api/src/main/java/de/adorsys/dfs/connection/api/complextypes/BucketPathUtil.java
@@ -1,6 +1,8 @@
 package de.adorsys.dfs.connection.api.complextypes;
 
 import de.adorsys.common.exceptions.BaseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +12,9 @@ import java.util.StringTokenizer;
  * Created by peter on 20.02.18 at 08:37.
  */
 public class BucketPathUtil {
+    private final static String WINDOWS_BUCKET_PATH_SEPARATOR = "\\\\";
+    private final static Logger LOGGER = LoggerFactory.getLogger(BucketPathUtil.class);
+
     public static String getAsString(BucketDirectory bucketDirectory) {
         String container = bucketDirectory.getObjectHandle().getContainer();
         String name = bucketDirectory.getObjectHandle().getName();
@@ -44,6 +49,7 @@ public class BucketPathUtil {
         if (fullBucketPath == null) {
             return list;
         }
+        fullBucketPath = fullBucketPath.replaceAll(WINDOWS_BUCKET_PATH_SEPARATOR, BucketPath.BUCKET_SEPARATOR);
         StringTokenizer st = new StringTokenizer(fullBucketPath, BucketPath.BUCKET_SEPARATOR);
         while (st.hasMoreElements()) {
             String token = st.nextToken();
