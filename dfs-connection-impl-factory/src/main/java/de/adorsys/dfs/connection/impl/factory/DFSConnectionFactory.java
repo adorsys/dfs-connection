1550947833fe26b15ea4add728ff96c1022ec164
pspex <pspex@adorsys.de>
2019-05-02 17:19:28 +0200

    DOC-98 make DFSConnectionFactory no more a singleton for easer mocking

--- dfs-connection-impl-factory/src/main/java/de/adorsys/dfs/connection/impl/factory/DFSConnectionFactory.java
+++ dfs-connection-impl-factory/src/main/java/de/adorsys/dfs/connection/impl/factory/DFSConnectionFactory.java
@@ -7,17 +7,32 @@ import de.adorsys.dfs.connection.api.types.properties.AmazonS3ConnectionProperti
 import de.adorsys.dfs.connection.api.types.properties.ConnectionProperties;
 import de.adorsys.dfs.connection.api.types.properties.FilesystemConnectionProperties;
 import de.adorsys.dfs.connection.impl.amazons3.AmazonS3DFSConnection;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Created by peter on 15.03.18 at 11:38.
  */
+@Slf4j
 public class DFSConnectionFactory {
-    private final static Logger LOGGER = LoggerFactory.getLogger(DFSConnectionFactory.class);
-    private static ConnectionProperties properties = null;
+    private final Logger LOGGER = LoggerFactory.getLogger(DFSConnectionFactory.class);
+    private ConnectionProperties properties = null;
+
+    public DFSConnectionFactory() {
+        log.debug("create new DFSConnectionFactory");
+    }
+
+    public static DFSConnection get() {
+        return new DFSConnectionFactory().getDFSConnection();
+    }
 
     public static DFSConnection get(ConnectionProperties properties) {
+        return new DFSConnectionFactory().getDFSConnection(properties);
+    }
+
+    public DFSConnection getDFSConnection(ConnectionProperties properties) {
+        log.debug("call getDFSConnection");
         if (properties instanceof AmazonS3ConnectionProperties) {
             return new AmazonS3DFSConnection((AmazonS3ConnectionProperties) properties);
         }
@@ -27,22 +42,19 @@ public class DFSConnectionFactory {
         throw new BaseException("Properties of unknown type: " + properties.getClass().getName());
     }
 
-    public static DFSConnection get() {
+    public DFSConnection getDFSConnection() {
         if (properties == null) {
             properties = new ReadArguments().readEnvironment();
         }
-        return get(properties);
+        return getDFSConnection(properties);
     }
 
-    public static void reset() {
-        properties = null;
-    }
 
     /**
      * @param args
      * @return die Argumente, die nicht verwertet werden konnten
      */
-    public static String[] readArguments(String[] args) {
+    public String[] readArguments(String[] args) {
         ReadArguments.ArgsAndProperties argsAndProperties = new ReadArguments().readArguments(args);
         properties = argsAndProperties.properties;
         return argsAndProperties.remainingArgs;
